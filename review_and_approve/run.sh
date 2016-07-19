0a0c93031f457aee1fd0af5bf8db77a9a8485e85
Bob Clarke <bob.m.clarke@gmail.com>
2016-07-19 17:14:59 +0300

    removed unwanted libs and classes

--- review_and_approve/run.sh
+++
@@ -1,3 +0,0 @@
-export AH_WEB_URL=https://localhost:8443
-export AGENT_HOME=/Users/clarkeb/urbancode/agent
-groovy -cp classes:lib/uDeployRestClient.jar:lib/jettison-1.1.jar lbgCreateSnapshot.groovy in.props out.props
