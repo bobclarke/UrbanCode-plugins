2c67322c02d64b996ccd3835eab7eb8c198d8fa8
Bob Clarke <bob.m.clarke@gmail.com>
2016-07-19 17:12:33 +0300

    removed unwanted libs and classes

--- review_and_approve/lbgCreateSnapshot.groovy
+++ review_and_approve/lbgCreateSnapshot.groovy
@@ -1,4 +1,9 @@
- 
+// --------------------------------------------------
+// lbgCreateSnapshot.groovy
+// Author: Bob Clarke
+// Date: 05/07/2016
+// --------------------------------------------------
+
 import com.urbancode.air.AirPluginTool
 import com.urbancode.ud.client.*
 import org.apache.http.HttpResponse
@@ -17,9 +22,10 @@ snapshotDescription = "Test"
 weburl = System.getenv("AH_WEB_URL")
 udUser = apTool.getAuthTokenUsername()
 udPass = apTool.getAuthToken()
+pluginDev = System.getenv("PLUGIN_DEV")
 
 // Check if we're being invoked from UrbanCode or via a command line test rig
-if (udUser.equals("PasswordIsAuthToken")){
+if (pluginDev.equals("yes")){
 	udUser = props['udUser']
 	udPass = props['udPass']
 }
@@ -29,9 +35,12 @@ if (udUser.equals("PasswordIsAuthToken")){
 // Main
 // --------------------------------------------------
 LBGUCRestClient restClient = new LBGUCRestClient(new URI(weburl), udUser, udPass)
-def snapshotNames = restClient.getSnapshotNames(applicationName)
+def applicationId = restClient.getApplicationId(applicationName)
+def snapshotNames = restClient.getSnapshotNames(applicationId)
 def newSnapshotName = getNewSnapshotName(snapshotNames, snapshotPrefix)
-println "Setting new snapshot name to "+newSnapshotName
+String newSnapshotId = restClient.createIbSnapshot(applicationName, environmentName, newSnapshotName)
+restClient.lockSnapshotVersions(applicationName, newSnapshotName)
+restClient.softLockSnaphotConfig(newSnapshotId)
  
 // --------------------------------------------------
 // Classes and Functions
@@ -42,9 +51,25 @@ public class LBGUCRestClient extends UDRestClient {
 		super(url, clientUser, clientPassword)
     	}
 
-	public getSnapshotNames(String applicationName){
-		String url = "${url}/cli/application/snapshotsInApplication?application=${applicationName}"
-		HttpGet method = new HttpGet(url)
+	public  getApplicationId(String applicationName){
+		String endpoint = "${url}/cli/application/info?application=${applicationName}"
+		println "\nGetting ID of application: ${applicationName}"
+		println "REST call is: ${endpoint}"
+		HttpGet method = new HttpGet(endpoint)
+		HttpResponse response = invokeMethod(method)
+		String body = getBody(response)
+		def slurper = new JsonSlurper()
+		def json = slurper.parseText(body.toString())
+		println "ID is ${json.id}"
+		return(json.id)
+	}
+
+	public getSnapshotNames(String applicationId){
+		//String endpoint = "${url}/cli/application/snapshotsInApplication?application=${applicationName}"
+		println "Getting existing Snapshot names"
+		String endpoint = "${url}/rest/deploy/application/${applicationId}/snapshots/false"
+		println "REST call is: ${endpoint}"
+		HttpGet method = new HttpGet(endpoint)
 		HttpResponse response = invokeMethod(method)
 		String body = getBody(response)
 		def slurper = new JsonSlurper()
@@ -52,16 +77,46 @@ public class LBGUCRestClient extends UDRestClient {
 		return(json.name)
 	}
 		
+	public createIbSnapshot(String applicationName, environmentName, snapshotName){
+		String endpoint = "${url}/cli/snapshot/createSnapshotOfEnvironment?application=${applicationName}&environment=${environmentName}&name=${snapshotName}"
+		println "Creating Snapshot"
+		println "REST call is: ${endpoint}"
+		HttpPut method = new HttpPut(endpoint)
+		HttpResponse response = invokeMethod(method)
+		String body = getBody(response)
+		def slurper = new JsonSlurper()
+		def json = slurper.parseText(body.toString())
+		def snapshotId = json.id
+		return(snapshotId)
+	}
+		
+	public lockSnapshotVersions(String applicationName, String snapshotName){
+		println "\nLocking snapshot versions"
+		String endpoint = "${url}/cli/snapshot/lockSnapshotVersions?application=${applicationName}&snapshot=${snapshotName}"
+		println "REST call is: ${endpoint}"
+		HttpPut method = new HttpPut(endpoint)
+		HttpResponse response = invokeMethod(method)
+	}
+
+	public softLockSnaphotConfig(String snapshotId){
+		println "\nApplying soft lock to snapshot configuration. ID is ${snapshotId}"
+		String endpoint = "${url}/rest/deploy/snapshot/${snapshotId}/configuration/lockAllToCurrent"
+		println "REST call is: ${endpoint}"
+		HttpPut method = new HttpPut(endpoint)
+		HttpResponse response = invokeMethod(method)
+	}
+		
 }
 
 String getNewSnapshotName(snapshotNames, snapshotPrefix){
-        def ninorVersions = []
-        def currentMinorVrsion
+	println "\nGenerating new snapshot name"
+        def minorVersions = []
+        def currentMinorVersion
         
         def namesArray = snapshotNames.findAll{ it.contains(snapshotPrefix)}
                
         namesArray.each{ name ->
-                minorVersion = name.split('_')[4]
+                minorVersion = name.split('_')[4].toInteger()
                 minorVersions.push(minorVersion)
         }
  
@@ -71,9 +126,12 @@ String getNewSnapshotName(snapshotNames, snapshotPrefix){
         }else{
                 currentMinorVersion =  minorVersions.max()
         }
+	println "Current minor version is ${currentMinorVersion}"
+	
         newMinorVersion = currentMinorVersion.toInteger()
         newMinorVersion++
         def newSnapshotName = snapshotPrefix+'_'+newMinorVersion
+	println "New snapshot name is ${newSnapshotName}"
         return(newSnapshotName)
 }
 
---
+++ review_and_approve/run_approvesnapshot.sh
@@ -0,0 +1,3 @@
+export AH_WEB_URL=https://localhost:8443
+groovy -cp classes:lib/uDeployRestClient.jar lbgApproveSnapshot.groovy approvesnapshot.props out.props
+
