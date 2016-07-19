cf379e9e2b285ec79c92478bcc4ef19e32faf7c2
Bob Clarke <bob.m.clarke@gmail.com>
2016-07-19 17:17:10 +0300

    removed unwanted libs and classes

--- review_and_approve/run_approvesnapshot.sh
+++
@@ -1,3 +0,0 @@
-export AH_WEB_URL=https://localhost:8443
-groovy -cp classes:lib/uDeployRestClient.jar lbgApproveSnapshot.groovy approvesnapshot.props out.props
-
--- review_and_approve/run_createsnapshot.sh
+++
@@ -1,3 +0,0 @@
-export AH_WEB_URL=https://localhost:8443
-groovy -cp classes:lib/uDeployRestClient.jar lbgCreateSnapshot.groovy createsnapshot.props out.props
-
